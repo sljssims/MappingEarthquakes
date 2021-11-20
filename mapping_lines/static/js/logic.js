1fd81b0cf28266ffbe3b76f42fcd09017a1b9443
sljssims <sljssims@gmail.com>
2021-11-20 13:43:58 -0600

    updated setview

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 
 // Create the map object with a center and zoom level. (Recenter setview as points change)
 // 1. Change the zoom to 7 in setView()
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
 // ///// Mapping Lines (Single Line)
 // // 2. Add the code for the line variable
@@ -36,47 +36,10 @@ L.polyline(line, {
  }).addTo(map);
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
-
-// ///Adding Multiple Markers using cities  (For Larger Files refer to an external file)  Commented out refer to external 'cities.js'
-// // An array containing each city's location, state, and population.
-// let cities = [{
-//     location: [40.7128, -74.0059],
-//     city: "New York City",
-//     state: "NY",
-//     population: 8398748
-//   },
-//   {
-//     location: [41.8781, -87.6298],
-//     city: "Chicago",
-//     state: "IL",
-//     population: 2705994
-//   },
-//   {
-//     location: [29.7604, -95.3698],
-//     city: "Houston",
-//     state: "TX",
-//     population: 2325502
-//   },
-//   {
-//     location: [34.0522, -118.2437],
-//     city: "Los Angeles",
-//     state: "CA",
-//     population: 3990456
-//   },
-//   {
-//     location: [33.4484, -112.0740],
-//     city: "Phoenix",
-//     state: "AZ",
-//     population: 1660272
-//   }
-//   ];
-
-
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // Get data from cities.js
 let cityData = cities;
-
 
 // Adding markers to the map
 // 1. Add the location of each city on the map using the marker() function and for loop
@@ -93,8 +56,6 @@ cityData.forEach(function(city) {
     .addTo(map);
 });
 
-
-
 /// Iterate through the array of cities to add each marker on the map
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
@@ -105,9 +66,6 @@ cities.forEach(function(city) {
 // We create the tile layer that will be the background of our map.
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
