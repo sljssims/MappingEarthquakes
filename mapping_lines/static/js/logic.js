f3599f0798cf5885e2a5fdcd1b03f966cc0dc38d
sljssims <sljssims@gmail.com>
2021-11-20 14:21:45 -0600

    commit fixes

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -2,9 +2,8 @@
 console.log("working");
 
 
-// Create the map object with a center and zoom level. (Recenter setview as points change)
-// 1. Change the zoom to 7 in setView()
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+// Create the map object with a center and zoom level.
+var map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 // ///// Mapping Lines (Single Line)
 // // 2. Add the code for the line variable
@@ -35,8 +34,7 @@ L.polyline(line, {
     color: "yellow"
  }).addTo(map);
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
 
 // Get data from cities.js
 let cityData = cities;
