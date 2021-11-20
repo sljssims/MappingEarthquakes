7af82e693c429a760c8ed7e3a278f57fabc2e532
sljssims <sljssims@gmail.com>
2021-11-20 11:58:00 -0600

    adding code for mapping single points

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -2,35 +2,71 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+
+// ///Adding Multiple Markers using cities  (For Larger Files refer to an external file)  Commented out refer to external 'cities.js'
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
 
 
-/////To add multiple variables like tile layers or background image, use the following template instead
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+// Get data from cities.js
+let cityData = cities;
 
-//  Add a marker (Default dot) to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Adding markers to the map
+// 1. Add the location of each city on the map using the marker() function and for loop
+// 2. Loop through the cities array and create one marker for each city
+// 3. Add a pop up marker for each marker using bindPopup() method 
+// 4. Format population by chaining 'city.population' to '.toLocaleString()'
+// 5. Replace the circle function with circleMarker to add more cicle attributes // Adjust the radius decrease each city's radius to fit (city.population/100000)
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
-// Add a circle with radius as a marker ()
-// L.circle([34.0522, -118.2437], {
-//     radius: 100
-//  }).addTo(map);
 
- ///Alternatively you can use the cirleMarker function for more detail
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
- }).addTo(map);
+/// Iterate through the array of cities to add each marker on the map
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });  
+
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
