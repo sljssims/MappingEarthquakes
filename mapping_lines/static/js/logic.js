455cc5adbc0a1eb21457451e90336cf0d669be43
sljssims <sljssims@gmail.com>
2021-11-20 13:34:27 -0600

    updating logic

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-<<<<<<< HEAD
+
 // Create the map object with a center and zoom level. (Recenter setview as points change)
 // 1. Change the zoom to 7 in setView()
 let map = L.map('mapid').setView([37.6213, -122.3790], 5);
@@ -34,7 +34,7 @@ let line = [
 L.polyline(line, {
     color: "yellow"
  }).addTo(map);
-=======
+
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
@@ -72,14 +72,12 @@ let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 //   }
 //   ];
 
->>>>>>> 3fa415a2d1e6448c21acf2a14392438552fc4146
+
 
 // Get data from cities.js
 let cityData = cities;
 
-<<<<<<< HEAD
 
-=======
 // Adding markers to the map
 // 1. Add the location of each city on the map using the marker() function and for loop
 // 2. Loop through the cities array and create one marker for each city
@@ -94,7 +92,7 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
 });
->>>>>>> 3fa415a2d1e6448c21acf2a14392438552fc4146
+
 
 
 /// Iterate through the array of cities to add each marker on the map
@@ -105,11 +103,11 @@ cities.forEach(function(city) {
 
 
 // We create the tile layer that will be the background of our map.
-<<<<<<< HEAD
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-=======
+
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
->>>>>>> 3fa415a2d1e6448c21acf2a14392438552fc4146
+
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
