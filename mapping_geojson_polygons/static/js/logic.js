d83c8a4d3c0e08dae34b64c87db9db876169f7c1
sljssims <sljssims@gmail.com>
2021-11-20 19:40:07 -0600

    polygon code

--- mapping_geojson_polygons/static/js/logic.js
+++ mapping_geojson_polygons/static/js/logic.js
@@ -9,8 +9,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 
-// We create the dark view tile layer that will be an option for our map. (Map2)
-let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the dark view tile layer that will be the background of our map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -35,16 +35,24 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the airport GeoJSON URL
 let torontoHoods = "https://raw.githubusercontent.com/sljssims/MappingEarthquakes/main/torontoNeighborhoods.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 1
+}
 
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data) {
   console.log(data);
 
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+   // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer){
+      layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+  }
+}).addTo(map);
 });
-
-
 
 
 
